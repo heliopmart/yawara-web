d0ad42a29f69416da10324abdf43ee81ad562457
Hélio Martins <helioperesmartinsneto@gmail.com>
2026-01-25 20:27:30 -0300

    Transfer of cron jobs from Vercel to Upstash Qstash

--- apps/frontend/src/app/api/admin/cron/process-editions/route.ts
+++ apps/frontend/src/app/api/admin/cron/process-editions/route.ts
@@ -2,18 +2,20 @@ import { NextResponse } from 'next/server';
 import { handle_error } from '@/utils/error'
 import { successResponse, errorResponse } from '@/lib/helpers/response';
 import { PsService } from '@/lib/services/ps/ps.service';
+import { Receiver } from "@upstash/qstash";
 import { TokenPayload } from '@yawara/types';
 
-export async function GET(request: Request) {
-    const authHeader = request.headers.get('authorization');
-    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-        return new NextResponse('Unauthorized', { status: 401 });
-    }
+const receiver = new Receiver({
+  currentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY!,
+  nextSigningKey: process.env.QSTASH_NEXT_SIGNING_KEY!,
+});
 
+async function handler(request: Request) {
     try {
         const adminPayload: TokenPayload = {
             user_id: 'cron-job-system',
             role: 'LEADER',
+            nuclei_id: 'cron-job-nuclei',
             secret: 'cron-job-secret',
             supabaseToken: process.env.SUPABASE_SERVICE_ROLE_KEY
         };
@@ -23,14 +25,12 @@ export async function GET(request: Request) {
 
         if(result === 'NOT_YET_TIME') {
             return successResponse<string>('No editions to process at this time.', 200);
-        }else if(result === 'NO_ACTIVE_EDITION') {
+        } else if(result === 'NO_ACTIVE_EDITION') {
             return successResponse<string>('No active editions found to process.', 200);
         }
 
         return successResponse<string>(result, 200);
     } catch (error) {
-        console.error('admin/ps/route.GET error:', error);
-
         const errorDetail = handle_error(error);
         return errorResponse(
             errorDetail.message,
@@ -38,4 +38,20 @@ export async function GET(request: Request) {
             errorDetail.statusCode,
         );
     }
+}
+
+export async function POST(request: Request) {
+    const signature = request.headers.get("upstash-signature");
+    const body = await request.text();
+
+    const isValid = await receiver.verify({
+        signature: signature!,
+        body: body,
+    });
+
+    if (!isValid) {
+        return new NextResponse('Invalid signature', { status: 401 });
+    }
+
+    return handler(request);
 }

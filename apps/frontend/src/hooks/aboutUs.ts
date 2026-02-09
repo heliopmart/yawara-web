a9794e221288bbad9ce8bb23fbf0c75452e9eea6
Hélio Martins <helioperesmartinsneto@gmail.com>
2026-02-09 14:42:56 -0300

    Fix<turn aboutUs server-only>

--- apps/frontend/src/hooks/aboutUs.ts
+++ apps/frontend/src/hooks/aboutUs.ts
@@ -1,33 +1,12 @@
-import { AboutUsData } from '@yawara/types';
+import "server-only";
+import { AboutUsService } from "@/lib/services/aboutUs/aboutUs.service"
+import { unstable_cache } from "next/cache";
 
-async function getAboutUsData(): Promise<AboutUsData> {
-    try{
-        const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/aboutUs`, {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
-            next: { revalidate: 3600 * 24 * 15 }
-        });
-
-        if (!res.ok) {
-            throw 'Erro ao buscar dados do About Us';
-        }
-
-        const data = await res.json();
-
-        if (!data.success) {
-            throw data.error.message
-        }
-
-        return data.data as AboutUsData;
-    }catch(error){
-        console.error('Erro ao buscar dados do About Us:', error);
-        return {
-            nuclei: [],
-            members: []
-        }
-    }
-}
-
+export const getAboutUsData = unstable_cache(
+  async () => new AboutUsService().getAboutUsData(),
+  ["aboutUsData"],
+  { revalidate: 3600 * 24 * 15 }
+);
 export const useAboutUs = async () => {
     const aboutUsData = await getAboutUsData();
 
